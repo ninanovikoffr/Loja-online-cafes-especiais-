469454e8a03ae24196cea145ebdc26e94c041e20
Gustavo-Martins610 <gustavo981233@gmail.com>
2025-11-30 18:02:18 -0300

    ADD:adicionando coluna total na tabela de pedidos

--- BackEnd/src/main/resources/db/migration/V3__criar__tabela_pedidos.sql
+++ BackEnd/src/main/resources/db/migration/V3__criar__tabela_pedidos.sql
@@ -3,7 +3,7 @@ CREATE TABLE pedidos (
     id_usuario BIGINT,
     id_endereco BIGINT,
     status VARCHAR(20),
-    created_at TIMESTAMP,
+    total DECIMAL(10,2),
     CONSTRAINT fk_pedido_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
     CONSTRAINT fk_pedido_endereco FOREIGN KEY (id_endereco) REFERENCES enderecos(id_endereco) ON DELETE CASCADE
 );
