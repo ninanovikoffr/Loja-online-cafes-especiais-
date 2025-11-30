0b61e2e146dca905afd6005bddc7c9063fe5acf8
Nina <ninanovikoffr@gmail.com>
2025-11-29 23:49:35 -0300

    Finalização Front end

--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,28 @@
-import { useNavigate, useLocation } from 'react-router-dom';
-import React from "react";
-import "./Navbar.css";
-
+import { useLocation, Link } from 'react-router-dom';
 import logo from "../../assets/Logo.svg";
-import perfil_icon from "../../assets/Perfil_icon.svg";
+import perfil_icon from "../../assets/Perfil_icon.svg"; // Ícone genérico de perfil
+import perfil_admin from "../../assets/Foto_admin.svg"; // Foto do admin para a tela de admin
 
 import { FaBars, FaSearch } from "react-icons/fa";
-import { Link } from 'react-router-dom';
 
 export function Navbar() {
+  const location = useLocation(); // Captura a localização atual (URL)
+
   return (
     <header className="navbar">
       <div className="navbar__left">
-        <img src={perfil_icon} className="navbar__icon" href="/Pages/Tela_admin/Tela_admin" alt="Perfil" />
-        <Link to="/login" className="navbar__login">Entrar</Link>
+        {/* Condicional para exibir o ícone de perfil genérico ou foto do admin */}
+        {location.pathname === '/admin' ? (
+          <>
+            <img src={perfil_admin} className="navbar__icon-admin" alt="Perfil Admin" />
+            <Link to="/admin" className="navbar__login">Meu Perfil</Link>
+          </>
+        ) : (
+          <>
+            <img src={perfil_icon} className="navbar__icon" alt="Perfil" />
+            <Link to="/login" className="navbar__login">Entrar</Link>
+          </>
+        )}
       </div>
 
       <div className="navbar__center">
