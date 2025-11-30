429b9ce31a18d322102cca444adb1a8d376b3b37
Nina <ninanovikoffr@gmail.com>
2025-11-30 16:17:29 -0300

    Ultimo ajuste navbar/integração navbar

--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,80 @@
-import { useNavigate, useLocation } from 'react-router-dom';
-import React from "react";
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 import logo from "../../assets/Logo.svg";
-import perfil_icon from "../../assets/Perfil_icon.svg";
+import perfil_icon from "../../assets/Perfil_icon.svg";      // cliente
+import perfil_admin from "../../assets/Foto_admin.svg";       // admin
 
 import { FaBars, FaSearch } from "react-icons/fa";
 
 export function Navbar() {
   const navigate = useNavigate();
-  
+
+  const [isLogged, setIsLogged] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const roleRaw = localStorage.getItem("role");
+    let roles = [];
+
+    try {
+      roles = roleRaw ? JSON.parse(roleRaw) : [];
+    } catch {
+      roles = [];
+    }
+
+    setIsLogged(!!token);
+    setIsAdmin(roles.includes("ADMIN"));
+  }, []);
+
+  const handleClick = () => {
+    if (!isLogged) {
+      navigate("/login");
+      return;
+    }
+
+    if (isAdmin) {
+      navigate("/admin");
+      return;
+    }
+  };
+
   return (
     <header className="navbar">
       <div className="navbar__left">
-        <img src={perfil_icon} className="navbar__icon" alt="Perfil" />
-        <button className="navbar__login" onClick={() => navigate('/login')} >Entrar</button>
+
+        {isLogged && isAdmin ? (
+          <img
+            src={perfil_admin}
+            className="navbar__icon"
+            alt="Admin"
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate("/admin")}
+          />
+        ) : (
+          <img
+            src={perfil_icon}
+            className="navbar__icon"
+            alt="Perfil"
+            style={{ cursor: "pointer" }}
+            onClick={handleClick}
+          />
+        )}
+
+        
+        {!isLogged && (
+          <button className="navbar__login" onClick={() => navigate('/login')}>
+            Entrar
+          </button>
+        )}
+
+        {isLogged && (
+          <button className="navbar__login">
+            Minha Conta
+          </button>
+        )}
       </div>
 
       <div className="navbar__center">
